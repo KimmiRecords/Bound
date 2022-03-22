048e09280e705c9e16d27dccf5d330b8c402605c
KimmiRecords <diego.katabian@davinci.edu.ar>
2022-03-22 00:26:45 -0300

    Intente empezar el mainmenu

---
+++ FPS Horror - Copy/Assets/MainMenuSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSelector : MonoBehaviour
+{
+    public bool apa = true;
+    void Start()
+    {
+        //todavia 
+    }
+
+    void Update()
+    {
+        //arranca seleccionado Start
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            apa = !apa; //togglea entre selecciones
+        }
+    }
+}
