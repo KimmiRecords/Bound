add6ab8576f4c8e9f4dd4c26b87ee6f4d39cf749
FranSerra <francisco.serra@davinci.edu.ar>
2022-03-23 23:16:45 -0300

    Pick ups
    Subo los pick ups interactuables con "E", tiene raycast en la camara y cuando estas a 5 unidades de distancia los podes levantar, se puede usar para cualquier interaccion. jaja saudos xd

--- FPS Horror - Copy/Assets/Scripts/MouseLook.cs
+++ FPS Horror - Copy/Assets/Scripts/MouseLook.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity;
+    //controla la vista FP y ademas tiene raycast en la camara.
 
     public Transform playerBody;
+
+    public Camera fpsCamera = null;
+
+    public float mouseSensitivity = 100f;
+
+    public float pickUpDistance = 100f;
+
+    public Interactable sensedObj = null;
+
 
     float xRotation = 0f;
     void Start()
     {
+        //Hace que el cursor desaparezca.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -25,5 +35,37 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        Ray ray = fpsCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * pickUpDistance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, pickUpDistance))
+        {
+            //Si le pegamos a algo.
+            Interactable obj = hit.collider.GetComponent<Interactable>();
+            if (obj)
+            {
+                sensedObj = obj;
+            }
+            else
+            {
+                sensedObj = null;
+            }
+        }
+        else
+        {
+            //si no le pegamos a nada.
+            sensedObj = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && sensedObj)
+        {
+            //Muestra en consola el nombre el tipo y la cantidad de objetos interactuados.
+            Debug.LogFormat("Used {0} of Type {1} Amount: {2}", sensedObj.name, sensedObj.pickUpType, sensedObj.amount);
+            DestroyImmediate(sensedObj.gameObject);
+            sensedObj = null;
+        }
     }
 }
