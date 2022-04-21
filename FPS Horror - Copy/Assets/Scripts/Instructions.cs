bbc596080976a35f4b20030290a19c6301ec8760
KimmiRecords <diego.katabian@davinci.edu.ar>
2022-04-21 01:12:32 -0300

    ajustecito de texto y props
    solo hice un fadecito para el texto enlas instrus, y acomode unos props para mas placer

--- FPS Horror - Copy/Assets/Scripts/Instructions.cs
+++ FPS Horror - Copy/Assets/Scripts/Instructions.cs
@@ -14,31 +14,50 @@ public class Instructions : MonoBehaviour
 
     private Color controlsInitialColor;
     private Color objectiveRedInitialColor;
+    private Color creditsInitialColor;
 
     private bool instructionsSeen = false;
+    private float timer;
 
     void Start()
     {
+        timer = 0;
+
         controlsInitialColor = controls.color; //guardo el color inicial
         objectiveRedInitialColor = objectiveRed.color;
+        creditsInitialColor = credits.color;
 
         controls.color = Color.clear; //arrancan invisibles
         objectiveRed.color = Color.clear;
         objectiveWhite.color = Color.clear;
+        credits.color = Color.clear;
 
+        
     }
 
     void Update()
     {
+        timer += Time.deltaTime / 5;
+
+        if (!instructionsSeen)
+        {
+            credits.color = new Color(1, 1, 1, Mathf.Lerp(0, 1, timer));
+        }
+
         if (Input.anyKey)
         {
-            credits.color = Color.clear;
-
-            controls.color = controlsInitialColor;
-            objectiveRed.color = objectiveRedInitialColor;
-            objectiveWhite.color = Color.white;
-
             instructionsSeen = true;
+
+            timer = 0;
+        }
+
+        if (instructionsSeen)
+        {
+            credits.color = Color.clear; //apaga los credits
+
+            controls.color = new Color(controlsInitialColor.r, controlsInitialColor.g, controlsInitialColor.b, Mathf.Lerp(0, 1, timer)); //y muestra las instrus
+            objectiveRed.color = new Color(objectiveRedInitialColor.r, objectiveRedInitialColor.g, objectiveRedInitialColor.b, Mathf.Lerp(0, 1, timer-1));
+            objectiveWhite.color = new Color(1, 1, 1, Mathf.Lerp(0, 1, timer-1));
         }
 
         if (Input.GetKeyDown(KeyCode.E) && instructionsSeen == true)
