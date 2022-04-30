03152a39da57b9d027ae598dc4d987e9aec10d37
FranSerra <francisco.serra@davinci.edu.ar>
2022-04-29 23:39:06 -0300

    Fix de puertas Lvl de testeo
    Agregue un prop de consola con botones tambien al lvl de testeo

--- FPS Horror - Copy/Assets/Scripts/Interactable.cs
+++ FPS Horror - Copy/Assets/Scripts/Interactable.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public enum EnumPickUpType
 {
     //Enumerador Para regular que item levantas y que cantidad de cada uno
-    item_usb, item_hp, item_battery, trigger_reja, trigger_grav, solo_infoPopup, item_flashlight
+    item_usb, item_hp, item_battery, trigger_reja, trigger_grav, solo_infoPopup, item_flashlight, trigger_button
 }
 public class Interactable : MonoBehaviour
 {
