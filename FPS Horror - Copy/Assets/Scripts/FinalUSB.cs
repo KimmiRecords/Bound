56b129a9fa706a32f2c3ec2f0ae496c4155bfa06
KimmiRecords <diego.katabian@davinci.edu.ar>
2022-06-01 01:24:31 -0300

    ajustes audio video y gases toxicos nuevos

--- FPS Horror - Copy/Assets/Scripts/FinalUSB.cs
+++ FPS Horror - Copy/Assets/Scripts/FinalUSB.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class FinalUSB : MonoBehaviour
 {
     //este script agrega funciones especiales al ultimo USB que agarras
+    
+
     //por diego katabian
 
     public AlarmLight[] lights; //las luces que voy a iniciar
+
+    public ToxicGas[] toxicGases;
     
     void OnDestroy()
     {
@@ -16,12 +20,17 @@ public class FinalUSB : MonoBehaviour
             return;
         }
 
-        AudioManager.instance.StopAlarmaNorway();
+        AudioManager.instance.StopAlarmaNorway(); //reproduzco la alarma heavy
         AudioManager.instance.PlayAlarmaTriple();
 
         for (int i = 0; i < lights.Length; i++)
         {
-            lights[i].gameObject.SetActive(true);
+            lights[i].gameObject.SetActive(true); //prendo las luces de alarma
+        }
+
+        for (int i = 0; i < toxicGases.Length; i++)
+        {
+            toxicGases[i].gameObject.SetActive(true); //prendo los extra gases
         }
 
     }
